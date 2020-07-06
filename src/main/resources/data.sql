da7450c28349119f5ebe80fd35c7e62d22dcd1c0
Maxim Rodionov <rodionov.m.dev@gmail.com>
2020-07-06 12:44:46 +0300

    secured passwords

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,8 +6,8 @@ DELETE FROM user_roles;
 DELETE FROM users;
 
 INSERT INTO users (id, email, password) VALUES
-  (100000, 'user@yandex.ru', 'password'),
-  (100001, 'admin@gmail.com', 'admin');
+  (100000, 'user@yandex.ru', '$2a$08$QtgY0EZyruarCcfdU8E/hePeEHjfuyL9iFfwFW1mC.A2tDv9GMq3e'),
+  (100001, 'admin@gmail.com', '$2a$08$4hMIdF/hu1F45srb7QKSDut5sGyDF7cLIwBOjerEL5xNOCYlHUkCS');
 
 INSERT INTO user_roles (role, user_id) VALUES
   ('USER', 100000),
@@ -25,13 +25,12 @@ INSERT INTO menu (id, date, restaurant_id) VALUES
   (100007, CURRENT_DATE - INTEGER '1', 100004);
 
 INSERT INTO dish (id, name, price, menu_id) VALUES
-    (100008, 'First main', 100.5, 100005),
-    (100009, 'First best', 50.3, 100005),
-    (100010, 'First cheap', 10.4, 100005),
-    (100011, 'Second main', 1000.9, 100006),
-    (100012, 'Second cheap', 200.0, 100006);
+    (100008, 'First main', 100, 100005),
+    (100009, 'First best', 50, 100005),
+    (100010, 'First cheap', 10, 100005),
+    (100011, 'Second main', 1000, 100006),
+    (100012, 'Second cheap', 200, 100006);
 
-INSERT INTO vote (id, user_id, menu_id, vote_date, vote_time) VALUES
-    (100013, 100000, 100005, CURRENT_DATE, '10:00:00'),
-    (100014, 100001, 100005, CURRENT_DATE, '12:00:00'),
-    (100015, 100000, 100007, CURRENT_DATE - INTEGER '1', '17:00:00');
+INSERT INTO vote (id, user_id, menu_id, vote_date) VALUES
+    (100014, 100001, 100005, CURRENT_DATE),
+    (100015, 100000, 100007, CURRENT_DATE - INTEGER '1');
