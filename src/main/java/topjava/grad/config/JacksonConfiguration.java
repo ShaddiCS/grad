bfadb3ad0d7b5345b805514b3d8fea94764f2258
Maxim Rodionov <rodionov.m.dev@gmail.com>
2020-08-26 17:18:31 +0300

    jackson mapper config

--- src/main/java/topjava/grad/config/JacksonConfiguration.java
+++ src/main/java/topjava/grad/config/JacksonConfiguration.java
@@ -1,14 +1,38 @@
 package topjava.grad.config;
 
-import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class JacksonConfiguration{
+public class JacksonConfiguration {
+
+    private static ObjectMapper mapper;
+
     @Bean
-    public Module objectMapper() {
-        return new Hibernate5Module();
+    public ObjectMapper objectMapper() {
+        mapper = new ObjectMapper();
+
+        mapper.registerModule(new Hibernate5Module());
+        mapper.registerModule(new JavaTimeModule());
+
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+        return mapper;
+    }
+
+    public static ObjectMapper getMapper() {
+        return mapper;
     }
 }
