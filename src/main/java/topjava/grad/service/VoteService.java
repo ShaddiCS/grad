986bfeaed98d0d6cef4cc70d7ddefb9e3e1b5ecd
Maxim Rodionov <rodionov.m.dev@gmail.com>
2020-08-26 17:35:56 +0300

    vote service impl changes

--- src/main/java/topjava/grad/service/VoteService.java
+++ src/main/java/topjava/grad/service/VoteService.java
@@ -20,18 +20,19 @@ public class VoteService {
 
     @Transactional
     public boolean updateVote(Integer restaurantId, User user) {
-        Vote voteFromDb = voteRepo.getByUser(user, LocalDate.now());
         Menu menu = menuRepo.getByDate(restaurantId, LocalDate.now());
 
         if (menu == null) {
             throw new NotFoundException("restaurantId=" + restaurantId);
         }
 
-        if (voteFromDb == null) {
+        Vote vote = voteRepo.getByUser(user, LocalDate.now()).orElse(null);
+
+        if (vote == null) {
             return false;
         }
 
-        voteFromDb.setMenu(menu);
+        vote.setMenu(menu);
         return true;
     }
 
@@ -43,23 +44,19 @@ public class VoteService {
             throw new NotFoundException("restaurantId=" + restaurantId);
         }
 
-        Vote vote = voteRepo.getByUser(user, LocalDate.now());
-
-        if (vote != null) {
+        if (voteRepo.getByUser(user, LocalDate.now()).isPresent()) {
             return false;
         }
 
-        voteRepo.save(new Vote(null, menu, user));
+        voteRepo.save(new Vote(menu, user));
         return true;
     }
 
     public Vote get(User user, LocalDate date) {
-        Vote vote = voteRepo.getByUser(user, date);
+        return voteRepo.getByUser(user, date).orElseThrow(() -> new NotFoundException("user=" + user.getUsername()));
+    }
 
-        if (vote == null) {
-            throw new NotFoundException("user=" + user.getUsername());
-        }
-
-        return vote;
+    public Vote getWithRestaurant(User user, LocalDate date) {
+        return voteRepo.getWithRestaurant(user, date).orElseThrow(() -> new NotFoundException("user=" + user.getUsername()));
     }
 }
